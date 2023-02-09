86a95df9ee14ed7c5b17370276c4faac5edff3ee
PvtPigeon <matiss.trams@gmail.com>
2023-02-10 00:21:41 +0200

    cleanup + formatting, button swapped just for link

--- src/components/buttons/CreateWishListRedirect.tsx
+++ src/components/buttons/CreateWishListRedirect.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export const CreateWishListRedirectButton = () => {
 
     return (
         <div className="flex mt-8">
-            <Link to={'/wishlist/create'}>
-                <button
-                    className="text-white px-8 py-2.5 rounded bg-sky-500 hover:bg-sky-600 transition font-semibold text-lg"
-                >
-                    Create a wish list!
-                </button>
+            <Link
+                className="text-white px-8 py-2.5 rounded bg-sky-500 hover:bg-sky-600 transition font-semibold text-lg"
+                to={'/wishlist/create'}
+            >
+                Create a wish list!
             </Link>
         </div>
     )
